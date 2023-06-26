ea4787f1988d27ebaa5e45631ae3faf7bfd18b4a
Serhii Dulin <64526933+SergDulin@users.noreply.github.com>
2023-06-27 01:04:12 +0300

    Створюємо функцію get_birthdays_per_week(users) Ця функція приймає список користувачів (users) і виводить на екран імена користувачів, у яких є дні народження наступного тижня, розподілені за днями тижня. 1. Викликає функцію get_period(), щоб отримати початок та кінець наступного тижня. 2. Ініціалізує порожній словник birthdays_per_day, який буде зберігати дні народження за днями тижня. 3. Для кожного користувача в списку users: Отримує дату народження користувача. 4. Порівнює місяць та число народження з початком та кінцем наступного тижня. Якщо дата народження попадає в цей діапазон, то додає ім'я користувача до відповідного дня тижня у словнику birthdays_per_day. 5. Виводить імена користувачів, розділені комами, для кожного дня тижня, використовуючи print().

--- goit_hw-08.py
+++ goit_hw-08.py
@@ -5,3 +5,20 @@ def get_period():
     start_of_next_week = current_date + timedelta(days=(7 - current_date.weekday()))
     end_of_next_week = start_of_next_week + timedelta(days=6)
     return start_of_next_week, end_of_next_week
+
+def get_birthdays_per_week(users):
+    start_of_next_week, end_of_next_week = get_period()
+    birthdays_per_day = {}
+
+    for user in users:
+        birthday = user['birthday'].date()
+
+        if start_of_next_week.month == birthday.month and start_of_next_week.day <= birthday.day <= end_of_next_week.day:
+            day_of_week = birthday.strftime('%A')
+            if day_of_week not in birthdays_per_day:
+                birthdays_per_day[day_of_week] = []
+            birthdays_per_day[day_of_week].append(user['name'])
+
+    for day_of_week, user_list in birthdays_per_day.items():
+        user_str = ', '.join(user_list)
+        print(f"{day_of_week}: {user_str}")
